35a7a728250c357ec06f57f704a1159e991bf294
unknown <raiquiem@gmail.com>
2024-10-03 01:18:09 -0400

    added submit button functionality, and template element

--- scripts/index.js
+++ scripts/index.js
@@ -28,6 +28,14 @@ const initialCards = [
 const profileEditButton = document.querySelector('#profile-edit-button'); 
 const profileEditModal = document.querySelector('#profile-edit-modal');
 const profileCloseButton = document.querySelector('#profile-close-button');
+const profileFormElement = document.querySelector('#modal-form');
+const nameInput = document.querySelector('#modal-title');
+const jobInput = document.querySelector('#modal-description');
+const profileName = document.querySelector('.profile__title');
+const jobName = document.querySelector('.profile__description');
+let cardTemplate = document.querySelector('#card');
+let cardTitle = document.querySelector('#card-title');
+let cardImage = document.querySelector('#card-image');
 
 profileEditButton.addEventListener('click', () => {
     profileEditModal.classList.add('modal_opened');
@@ -36,3 +44,18 @@ profileEditButton.addEventListener('click', () => {
 profileCloseButton.addEventListener('click', () => {
     profileEditModal.classList.remove('modal_opened');
 });
+
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault();
+
+    profileName.textContent = nameInput.value;
+    jobName.textContent = jobInput.value;
+  
+}
+
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
+
+function getCardElement(data) {
+    let cardElement = cardTemplate.querySelector('.card');
+
+}
