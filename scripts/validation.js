42b68040f4112c447a66723b1dfa5a82fd78ac6a
unknown <raiquiem@gmail.com>
2025-05-24 00:46:12 -0400

    feat: added validation, error message, eventlisteners, & button visibility

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,12 +1,86 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
+function showInputError(formEl,inputEl, {inputErrorClass, errorClass}) {
+    const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+    inputEl.classList.add(inputErrorClass);
+    errorMessageEl.textContext = inputEl.validationMessage;
+    errorMessageEl.classlist.add(errorClass);  
+}
+
+function hideInputError(formEl,inputEl, {inputErrorClass, errorClass}) {
+    const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+    inputEl.classList.remove(inputErrorClass);
+    errorMessageEl.textContext = "";
+    errorMessageEl.classlist.remove(errorClass);  
+}
+
+function checkInputValidity(formEl, inputEl, options) {
+    if (!inputEl.validity.valid) {
+        return showInputError(formEl, inputEl, options);
+    }
+    
+    hideInputError(formEl,inputEl, options);
+    
+}
+
+function hasInvalidInput(inputList) {
+    return !inputList.every((inputEl) => inputEl.validity.valid);
+
+}
+
+function disableButton(submitButton, {inactiveButtonClass}) {
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.disabled = true;
+    return;
+}
+
+function enableButton(submitButton, {inactiveButtonClass}) {
+    submitButton.classList.remove(inactiveButtonClass);
+    submitButton.disabled = false;
+}
+
+function togglebuttonState(inputEls) {
+    if(hasInvalidInput(inputEls)) {
+        disableButton();
+    } 
+
+    enableButton();    
+}
+
+function setEventListeners(formEl, options) {
+    const {inputSelector} = options;
+    const inputEls = [...formEl.querySelectorAll(inputSelector)];
+    const submitButton = (formEl.querySelector('.popup__button'));
+    inputEls.forEach((inputEl) => {
+        inputEl.addEventListener("input", () => {
+            checkInputValidity(formEl, inputEl, options);
+            togglebuttonState(inputEls, submitButton, options);
+
+        })
+    })
+
+}
+
 function enableValidation(options) {
-  const formEls = Array.from(document.querySelectorAll(".form"));
-  formEls.forEach((formElement) => {
-    formElement.addEventListener("submit", (evt) => {
+  const formEls = [...document.querySelectorAll(options.formSelector)];
+  formEls.forEach((formEl) => {
+    formEl.addEventListener("submit", (evt) => {
       evt.preventDefault();
     });
+    
+    setEventListeners(formEl, options);
+
+    // look for all ionputs inside of form
+    // loop through all the inputs to see if all are valid
+        // if input is not valid 
+            // get validation message 
+            // add error class to input
+            // display error message
+            // disable button
+        // if all inputs are valid
+            // enable button
+            // reset error messages
 
 });
 }
